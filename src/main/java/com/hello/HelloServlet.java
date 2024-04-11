7c449849482b96865b8e38bc98f05ed769ecbfbe
Tetz <xenosign@naver.com>
2024-04-11 13:05:59 +0900

    240411 5강 매핑 끝

--- src/main/java/com/hello/HelloServlet.java
+++ src/main/java/com/hello/HelloServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet("/HelloServlet")
+@WebServlet("/HS1")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
